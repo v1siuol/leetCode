c54e5ba31deab94428ad20573766a15d987f4bd7
v1siuol <1098741925@qq.com>
2017-02-24 19:53:13 -0500

    Create E_344_ReverseString.py

---
+++ E_344_ReverseString.py
@@ -0,0 +1,16 @@
+"""
+476 / 476 test cases passed.
+Status: Accepted
+Runtime: 78 ms
+You are here!
+Your runtime beats 15.26% of python submissions.
+"""
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result = list(s)
+        result.reverse()
+        return "".join(result)
