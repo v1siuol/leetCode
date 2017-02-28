f0481605054cdb3882245fd6c5e0ede422be003c
v1siuol <1098741925@qq.com>
2017-02-27 19:21:30 -0500

    Create E_476_NumberComplement.py

---
+++ E_476_NumberComplement.py
@@ -0,0 +1,22 @@
+"""
+149 / 149 test cases passed.
+Status: Accepted
+Runtime: 45 ms
+You are here!
+Your runtime beats 47.91% of python submissions.
+"""
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        result = 0
+        count = 0
+        nTo2 = list(bin(num).replace("0b", ""))
+
+        while nTo2:
+            if nTo2.pop() == "0":
+                result += 2 ** count
+            count += 1
+        return result
