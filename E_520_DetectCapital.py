24a1676a90a97f92ccaed5bc941463a30aa57936
v1siuol <1098741925@qq.com>
2017-03-07 15:54:53 -0500

    Create E_520_DetectCapital.py

---
+++ E_520_DetectCapital.py
@@ -0,0 +1,20 @@
+"""
+550 / 550 test cases passed.
+Status: Accepted
+Runtime: 49 ms
+You are here!
+Your runtime beats 82.15% of python submissions.
+"""
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        if word.isupper():
+            return True
+        elif word.islower():
+            return True
+        elif word[0].isupper() and word[1:].islower():
+            return True
+        return False
