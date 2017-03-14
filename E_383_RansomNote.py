c200d77da3fa12a71e1c68aa924c117db2a448fa
v1siuol <1098741925@qq.com>
2017-03-14 05:10:08 -0400

    Create E_383_RansomNote.py

---
+++ E_383_RansomNote.py
@@ -0,0 +1,21 @@
+"""
+126 / 126 test cases passed.
+Status: Accepted
+Runtime: 159 ms
+You are here!
+Your runtime beats 47.04% of python submissions.
+"""
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        m = list(magazine)
+        for i in ransomNote:
+            try:
+                m.remove(i)
+            except ValueError:
+                return False
+        return True
