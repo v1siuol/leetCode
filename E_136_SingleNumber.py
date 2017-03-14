32eee2ba2f33c9b01093ff4d27abef8b1afff273
v1siuol <1098741925@qq.com>
2017-03-13 21:17:35 -0400

    Create E_136_SingleNumber.py

---
+++ E_136_SingleNumber.py
@@ -0,0 +1,22 @@
+"""
+15 / 15 test cases passed.
+Status: Accepted
+Runtime: 66 ms
+You are here!
+Your runtime beats 28.32% of python submissions.
+"""
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        i = 0
+        while i != len(nums) - 1:
+            if nums[i] != nums[i+1]:
+                return nums[i]
+            else:
+                i += 1
+            i += 1
+        return nums[-1]
