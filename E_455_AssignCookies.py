76b7b0b2fdad3d794bad72fada3346302bbb41aa
v1siuol <1098741925@qq.com>
2017-03-15 00:17:55 -0400

    Create E_455_AssignCookies.py

---
+++ E_455_AssignCookies.py
@@ -0,0 +1,28 @@
+"""
+21 / 21 test cases passed.
+Status: Accepted
+Runtime: 95 ms
+You are here!
+Your runtime beats 68.75% of python submissions.
+"""
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()
+        s.sort()
+        i = 0
+        c = 0
+        re = 0
+        while i < len(s):
+            if s[i] >= g[c]:
+                re += 1
+                c += 1
+                if c >= len(g):
+                    return re
+            i += 1
+        return re
+        
