b2850e8cde4ac6203d0cd426bd9a51fdef20d2b9
v1siuol <1098741925@qq.com>
2017-03-15 00:18:48 -0400

    Create E_506_RelativeRanks.py

---
+++ E_506_RelativeRanks.py
@@ -0,0 +1,28 @@
+"""
+17 / 17 test cases passed.
+Status: Accepted
+Runtime: 546 ms
+You are here!
+Your runtime beats 13.85% of python submissions.
+"""
+class Solution(object):
+    def findRelativeRanks(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        model = nums[:]
+        nums.sort(reverse=True)
+        re = []
+        for i in model:
+            v = nums.index(i)
+            if v == 0:
+                v = "Gold Medal"
+            elif v == 1:
+                v = "Silver Medal"
+            elif v == 2:
+                v = "Bronze Medal"
+            else:
+                v = str(v+1)
+            re.append(v)
+        return re
