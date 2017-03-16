b0ac004ea1feb77078f5644e46fdfc47f8dd0e53
v1siuol <1098741925@qq.com>
2017-03-16 00:40:31 -0400

    Create E_258_AddDigits.py

---
+++ E_258_AddDigits.py
@@ -0,0 +1,17 @@
+"""
+1101 / 1101 test cases passed.
+Status: Accepted
+Runtime: 52 ms
+You are here!
+Your runtime beats 46.13% of python submissions.
+"""
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        re = sum(map(int, list(str(num))))
+        while re >= 10:
+            re = sum(map(int, list(str(re))))
+        return re
