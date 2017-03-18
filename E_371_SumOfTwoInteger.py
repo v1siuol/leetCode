b199c10e784f6bc27b64809b5a024bb371e5192a
v1siuol <1098741925@qq.com>
2017-03-18 00:18:09 -0400

    Create E_371_SumOfTwoInteger.py

---
+++ E_371_SumOfTwoInteger.py
@@ -0,0 +1,15 @@
+"""
+13 / 13 test cases passed.
+Status: Accepted
+Runtime: 62 ms
+You are here!
+Your runtime beats 6.88% of python submissions.
+"""
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        return sum([a,b])
