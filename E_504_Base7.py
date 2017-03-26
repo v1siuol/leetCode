b5776e55c8dc5c66f0fdc678663be662888db358
v1siuol <1098741925@qq.com>
2017-03-25 23:22:20 -0400

    Create E_504_Base7.py

---
+++ E_504_Base7.py
@@ -0,0 +1,23 @@
+"""
+241 / 241 test cases passed.
+Status: Accepted
+Runtime: 42 ms
+You are here!
+Your runtime beats 73.34% of python submissions.
+"""
+class Solution(object):
+    def convertToBase7(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        re = []
+        temp = abs(num)
+        while temp > 6:
+            re.append(str(temp % 7))
+            temp //= 7
+        re.append(str(temp))
+        if num < 0:
+            re.append("-")
+        re.reverse()
+        return "".join(re)
