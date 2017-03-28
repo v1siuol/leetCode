50e9b53891359cc6bbc59707072e69af4cf34b9c
v1siuol <1098741925@qq.com>
2017-03-28 08:44:24 -0400

    Create E_104_MaximumDepthOfBinaryTree.py

---
+++ E_104_MaximumDepthOfBinaryTree.py
@@ -0,0 +1,23 @@
+"""
+38 / 38 test cases passed.
+Status: Accepted
+Runtime: 69 ms
+You are here!
+Your runtime beats 53.52% of python submissions.
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
