895b51d2bc0067946bfb24b3460cf090124a8247
v1siuol <1098741925@qq.com>
2017-03-31 00:15:54 -0400

    Create E_349_IntersectionOfTwoArrays.py

---
+++ E_349_IntersectionOfTwoArrays.py
@@ -0,0 +1,15 @@
+"""
+60 / 60 test cases passed.
+Status: Accepted
+Runtime: 65 ms
+You are here!
+Your runtime beats 34.34% of python submissions.
+"""
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1) & set(nums2))
