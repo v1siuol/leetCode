e2ac8fe7aa9ea5064d1c416dad6e274fc68b27ca
v1siuol <1098741925@qq.com>
2017-04-04 10:36:45 -0400

    Create E_217_ContainsDuplicate.py

---
+++ E_217_ContainsDuplicate.py
@@ -0,0 +1,14 @@
+"""
+18 / 18 test cases passed.
+Status: Accepted
+Runtime: 72 ms
+You are here!
+Your runtime beats 35.36% of python submissions.
+"""
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
