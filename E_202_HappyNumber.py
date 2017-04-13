d2e5085e8ea95fa4d5a6b8c15ed93ac597997d69
v1siuol <1098741925@qq.com>
2017-04-13 00:19:26 -0400

    Create E_202_HappyNumber.py

---
+++ E_202_HappyNumber.py
@@ -0,0 +1,23 @@
+"""
+401 / 401 test cases passed.
+Status: Accepted
+Runtime: 62 ms
+You are here!
+Your runtime beats 35.12% of python submissions.
+"""
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        n = abs(n)
+        lst = []
+        while n not in lst:
+            lst.append(n)
+            re = 0
+            while n > 0:
+                re += (n % 10) ** 2
+                n //= 10
+            n = re
+        return n == 1
