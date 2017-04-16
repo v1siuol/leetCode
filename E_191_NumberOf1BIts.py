66b70f6bfc4186c5e955934cc16cb7138954dcf8
v1siuol <1098741925@qq.com>
2017-04-15 23:07:52 -0400

    Create E_191_NumberOf1BIts.py

---
+++ E_191_NumberOf1BIts.py
@@ -0,0 +1,15 @@
+"""
+600 / 600 test cases passed.
+Status: Accepted
+Runtime: 45 ms
+You are here!
+Your runtime beats 63.55% of python submissions.
+"""
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        return bin(n).count("1")
