a1c8bcb3d1570b3da5aebfb44ffb43e4fdbb2490
v1siuol <1098741925@qq.com>
2017-04-17 00:52:02 -0400

    Create E_231_PowerOfTwo.py

---
+++ E_231_PowerOfTwo.py
@@ -0,0 +1,15 @@
+"""
+1108 / 1108 test cases passed.
+Status: Accepted
+Runtime: 52 ms
+You are here!
+Your runtime beats 49.18% of python submissions.
+"""
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        #print(bin(n))
+        return bin(n).count("1") == 1 if n > 0 else False
