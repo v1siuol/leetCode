4c88686c6177502d3c864bace0f50c0064057828
v1siuol <1098741925@qq.com>
2017-04-19 15:30:55 -0400

    Create E_35_SearchInsertPosition.py

---
+++ E_35_SearchInsertPosition.py
@@ -0,0 +1,36 @@
+"""
+62 / 62 test cases passed.
+Status: Accepted
+Runtime: 42 ms
+You are here!
+Your runtime beats 72.83% of python submissions.
+"""
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # binary search
+        low = 0
+        high = len(nums) - 1
+        mid = (low + high) >> 1
+        while True:
+            if target > nums[mid]:
+                low = mid
+                mid = (low + high) >> 1
+                if low == mid:
+                    while low < len(nums) and target > nums[low]:
+                        low += 1
+                    return low
+            elif target < nums[mid]:
+                high = mid
+                mid = (low + high) >> 1
+                if high == mid:
+                    while 0 < high and target < nums[high]:
+                        high -= 1
+                    return high
+            elif target == nums[mid]:
+                return mid
+        return -1
