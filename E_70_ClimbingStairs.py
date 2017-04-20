4fce034cc9ccdb85fdcff3c6b48f24467f244135
v1siuol <1098741925@qq.com>
2017-04-20 00:31:00 -0400

    Create E_70_ClimbingStairs.py

---
+++ E_70_ClimbingStairs.py
@@ -0,0 +1,24 @@
+"""
+45 / 45 test cases passed.
+Status: Accepted
+Runtime: 46 ms
+You are here!
+Your runtime beats 37.62% of python submissions.
+"""
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def jc(num):
+            r = 1
+            for i in range(2,num+1):
+                r *= i
+            return r
+        i = 2
+        re = 1
+        while i <= n:
+            re += (jc(n-i+(i>>1)) / jc(i>>1) / jc(n-i))
+            i += 2
+        return int(re)
