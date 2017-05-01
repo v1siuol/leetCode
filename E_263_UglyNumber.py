9923e8d4936a09616874d21c5a174af85a59bd76
v1siuol <1098741925@qq.com>
2017-04-30 22:49:37 -0400

    Create E_263_UglyNumber.py

---
+++ E_263_UglyNumber.py
@@ -0,0 +1,23 @@
+"""
+1012 / 1012 test cases passed.
+Status: Accepted
+Runtime: 48 ms
+You are here!
+Your runtime beats 70.88% of python submissions.
+"""
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+
+        while (num & 1) == 0:
+            num >>= 1
+        while (num % 5) == 0:
+            num //= 5
+        while (num % 3) == 0:
+            num //= 3
+        return num == 1
