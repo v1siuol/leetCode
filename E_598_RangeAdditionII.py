fc14651c754e373cc90f4a5ed9694fce0a84f3bd
v1siuol <1098741925@qq.com>
2017-06-29 23:30:30 +0800

    Create E_598_RangeAdditionII.py

---
+++ E_598_RangeAdditionII.py
@@ -0,0 +1,29 @@
+"""
+69 / 69 test cases passed.
+Status: Accepted
+Runtime: 46 ms
+You are here!
+Your runtime beats 72.20 % of python submissions.
+"""
+class Solution(object):
+    def maxCount(self, m, n, ops):
+        """
+        :type m: int
+        :type n: int
+        :type ops: List[List[int]]
+        :rtype: int
+        """
+        #if ops:
+        if ops[0]:
+            minX = ops[0][0]
+            minY = ops[0][1]
+            index = 1
+            while index < len(ops):
+                if ops[index][0] < minX:
+                    minX = ops[index][0]
+                if ops[index][1] < minY:
+                    minY = ops[index][1]
+                index += 1
+            return minX * minY
+        else:
+            return m * n
