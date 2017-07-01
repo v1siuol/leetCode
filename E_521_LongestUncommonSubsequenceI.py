0d21b173aeb611d27518bafd08a748e150b24746
v1siuol <1098741925@qq.com>
2017-07-01 23:55:39 +0800

    Create E_521_LongestUncommonSubsequenceI.py

---
+++ E_521_LongestUncommonSubsequenceI.py
@@ -0,0 +1,18 @@
+"""
+41 / 41 test cases passed.
+Status: Accepted
+Runtime: 39 ms
+You are here!
+Your runtime beats 64.46 % of python submissions.
+"""
+class Solution(object):
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        if a != b:
+            return max(len(a), len(b))
+        else:
+            return -1
