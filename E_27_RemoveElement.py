952d62fbc627cfa1ecd4251e4b101754e40d7ece
v1siuol <1098741925@qq.com>
2017-12-17 18:31:47 -0600

    2017.12.17

---
+++ E_27_RemoveElement.py
@@ -0,0 +1,23 @@
+"""
+113 / 113 test cases passed.
+Status: Accepted
+Runtime: 42 ms
+You are here!
+Your runtime beats 78.69 % of python submissions.
+"""
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        res = len(nums)
+        while i < len(nums):
+            if val == nums[i]:
+                nums.pop(i)
+                res -= 1
+            else:
+                i += 1
+        return res
